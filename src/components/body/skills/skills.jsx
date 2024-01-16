3d1f12e5a99f44d0d8989bb985baa42fc93bba28
DelrosarioRovic <rovicdelrosario01@gmail.com>
2024-01-16 11:24:37 +0800

    adding skills pictures

--- src/components/body/skills/skills.jsx
+++ src/components/body/skills/skills.jsx
@@ -14,6 +14,11 @@ import Node from "../../../image/node.png";
 import Mongo from "../../../image/mongo.png";
 import Github from "../../../image/github.png";
 import Typescript from "../../../image/typescript.png";
+import Shopify from "../../../image/shopify.png";
+import Remix from "../../../image/remix.png";
+import Jest from "../../../image/jest.svg";
+import Redux from "../../../image/redux.png";
+import Next from "../../../image/next.svg";
 
 function Skills() {
   const { letterClass, isHovering, handleMouseEnter, handleMouseLeave } =
@@ -43,45 +48,110 @@ function Skills() {
           //This is the TECNOLOGY I've work with 🧑‍💻
         </h3>
         <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8 text-white text-opacity-90">
-          <div data-aos="zoom-in" className="shadow-md shadow-[#06092e41] hover:!scale-110 duration-500">
+          <div
+            data-aos="zoom-in"
+            className="shadow-md shadow-[#06092e41] hover:!scale-110 duration-500"
+          >
             <img src={Html} alt="html" className="w-20 mx-auto" />
             <p>HTML</p>
           </div>
-          <div data-aos="zoom-in" className="shadow-md shadow-[#06092e41] hover:!scale-110 duration-500">
+          <div
+            data-aos="zoom-in"
+            className="shadow-md shadow-[#06092e41] hover:!scale-110 duration-500"
+          >
             <img src={Css} alt="css" className="w-20 mx-auto" />
             <p>CSS</p>
           </div>
-          <div data-aos="zoom-in" className="shadow-md shadow-[#06092e41] hover:!scale-110 duration-500">
+          <div
+            data-aos="zoom-in"
+            className="shadow-md shadow-[#06092e41] hover:!scale-110 duration-500"
+          >
             <img src={Bt} alt="css" className="w-20 mx-auto" />
             <p>BOOTSTRAP</p>
           </div>
-          <div data-aos="zoom-in" className="shadow-md shadow-[#06092e41] hover:!scale-110 duration-500">
+          <div
+            data-aos="zoom-in"
+            className="shadow-md shadow-[#06092e41] hover:!scale-110 duration-500"
+          >
             <img src={Js} alt="css" className="w-20 mx-auto" />
             <p>JAVASCRIPT</p>
           </div>
-          <div data-aos="zoom-in" className="shadow-md shadow-[#06092e41] hover:!scale-110 duration-500">
+          <div
+            data-aos="zoom-in"
+            className="shadow-md shadow-[#06092e41] hover:!scale-110 duration-500"
+          >
             <img src={Tailwind} alt="html" className="w-20 mx-auto" />
             <p>TAILWIND</p>
           </div>
-          <div data-aos="zoom-in" className="shadow-md shadow-[#06092e41] hover:!scale-110 duration-500">
+          <div
+            data-aos="zoom-in"
+            className="shadow-md shadow-[#06092e41] hover:!scale-110 duration-500"
+          >
             <img src={ReactP} alt="css" className="w-20 mx-auto" />
             <p>REACT</p>
           </div>
-          <div data-aos="zoom-in" className="shadow-md shadow-[#06092e41] hover:!scale-110 duration-500">
+          <div
+            data-aos="zoom-in"
+            className="shadow-md shadow-[#06092e41] hover:!scale-110 duration-500"
+          >
             <img src={Node} alt="css" className="w-20 mx-auto" />
             <p>NODE JS</p>
           </div>
-          <div data-aos="zoom-in" className="shadow-md shadow-[#06092e41] hover:!scale-110 duration-500">
+          <div
+            data-aos="zoom-in"
+            className="shadow-md shadow-[#06092e41] hover:!scale-110 duration-500"
+          >
             <img src={Mongo} alt="html" className="w-20 mx-auto" />
             <p>MONGO DB</p>
           </div>
-          <div data-aos="zoom-in" className="shadow-md shadow-[#06092e41] hover:!scale-110 duration-500">
+          <div
+            data-aos="zoom-in"
+            className="shadow-md shadow-[#06092e41] hover:!scale-110 duration-500"
+          >
             <img src={Github} alt="css" className="w-20 mx-auto" />
             <p>GITHUB</p>
           </div>
-          <div data-aos="zoom-in" className="shadow-md shadow-[#06092e41] hover:!scale-110 duration-500">
+          <div
+            data-aos="zoom-in"
+            className="shadow-md shadow-[#06092e41] hover:!scale-110 duration-500"
+          >
             <img src={Typescript} alt="css" className="w-20 mx-auto" />
             <p>Typescript</p>
+          </div>
+          <div
+            data-aos="zoom-in"
+            className="shadow-md shadow-[#06092e41] hover:!scale-110 duration-500"
+          >
+            <img src={Next} alt="css" className="w-20 mx-auto" />
+            <p>Next.Js</p>
+          </div>
+          <div
+            data-aos="zoom-in"
+            className="shadow-md shadow-[#06092e41] hover:!scale-110 duration-500"
+          >
+            <img src={Redux} alt="css" className="w-20 mx-auto" />
+            <p>Redux</p>
+          </div>
+          <div
+            data-aos="zoom-in"
+            className="shadow-md shadow-[#06092e41] hover:!scale-110 duration-500"
+          >
+            <img src={Jest} alt="css" className="w-20 mx-auto" />
+            <p>Jest</p>
+          </div>
+          <div
+            data-aos="zoom-in"
+            className="shadow-md shadow-[#06092e41] hover:!scale-110 duration-500"
+          >
+            <img src={Remix} alt="css" className="w-20 mx-auto" />
+            <p>Remix</p>
+          </div>
+          <div
+            data-aos="zoom-in"
+            className="shadow-md shadow-[#06092e41] hover:!scale-110 duration-500"
+          >
+            <img src={Shopify} alt="css" className="w-20 mx-auto" />
+            <p>Shopify</p>
           </div>
         </div>
       </div>
